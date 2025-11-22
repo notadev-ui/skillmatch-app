efe7bfd287bf8a69229230db8f6898f01b936c90
KmNeetuSingh <itisneetusingh@gmail.com>
2025-11-22 17:46:20 +0530

    vercel file added

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,12 @@ const io = new Server(httpServer, {
   }
 });
 
+const corsOptions = {
+  origin: '*'
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
